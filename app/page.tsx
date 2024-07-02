e99481222f9de8a05000de6ac52df33716323be9
JunichiAJISAKA <ajisaka@ajio.co.jp>
2024-07-02 15:16:29 +0900

    check window object

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 import type {NextPage} from "next";
-import React from "react";
+import React, {useEffect} from "react";
 import {ViewerWrapperProps} from "../components/ReportViewer";
 
 // 動的インポートを使用して、レポートビューワのラッパーをロードします。詳細については、「https://nextjs.org/docs/advanced-features/dynamic-import」を参照してください。
@@ -13,11 +14,17 @@ const Viewer = dynamic<ViewerWrapperProps>(
 );
 
 const Home: NextPage = () => {
+    const [canShow, setCanShow] = React.useState(false);
+    useEffect(() => {
+        if(!window || typeof window === 'undefined') return;
+        setCanShow(true);
+    },[]);
+
     return (
         <div
             style={{width: "100%", height: "100vh"}}
         >
-            {typeof window !== 'undefined' &&
+            {canShow &&
                 <Viewer reportUri="reports/Invoice_green_ipa.rdlx-json" language="ja"/>
             }
         </div>
